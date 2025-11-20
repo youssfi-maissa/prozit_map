1ee04046562a251d04f81749c5abeb8172f329db
youssfi-maissa <Maissa.Yousfi@esprit.tn>
2025-11-20 21:05:01 +0100

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,39 @@
+public class Main {
+    public static void main(String[] args) {
+
+        DepartementHashSet gestionDep = new DepartementHashSet();
+        AffectationHashMap affect = new AffectationHashMap();
+
+        Departement d1 = new Departement(1, "Informatique", 30);
+        Departement d2 = new Departement(2, "RH", 10);
+
+        Employe e1 = new Employe(101, "Ali", "Sami");
+        Employe e2 = new Employe(102, "Sara", "Nour");
+        Employe e3 = new Employe(103, "Moussa", "Karim");
+
+        gestionDep.ajouterDepartement(d1);
+        gestionDep.ajouterDepartement(d2);
+
+        affect.ajouterEmployeDepartement(e1, d1);
+        affect.ajouterEmployeDepartement(e2, d2);
+        affect.ajouterEmployeDepartement(e3, d1);
+
+        affect.afficherEmployesEtDepartements();
+
+        affect.ajouterEmployeDepartement(e1, d2);
+
+        System.out.println("-----");
+        affect.afficherEmployesEtDepartements();
+
+        affect.supprimerEmploye(e2);
+
+        System.out.println("-----");
+        affect.afficherEmployes();
+
+        System.out.println("-----");
+        affect.afficherDepartements();
+
+        System.out.println("-----");
+        System.out.println(affect.trierMap());
+    }
+}
